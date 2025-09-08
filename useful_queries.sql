f446834d596261be35e27bf7dde630d279e06020
Ethan Roman <eroman6302@gmail.com>
2025-09-08 14:38:17 -0400

    added queries

--- useful_queries.sql
+++ useful_queries.sql
@@ -10,3 +10,11 @@ ORDER BY utcdate DESC, utctime DESC;
 SELECT COUNT(*) AS total_count FROM games
 WHERE (white = "eroman5" OR black = "eroman5")
 AND opening IS NOT NULL;
+
+-- Count of wins for specific user grouped by opening
+SELECT COUNT(*) AS win_count
+FROM games
+WHERE ((white = 'eroman5' AND result = '1-0')
+    OR (black = 'eroman5' AND result = '0-1'))
+    AND opening IS NOT NULL
+GROUP BY opening;
